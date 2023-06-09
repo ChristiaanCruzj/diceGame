e913ff55f75988c224a5d008cb52664ca68f5702
ChristiaanCruzj <christiaancruzj@gmail.com>
2023-06-08 23:04:19 -0600

    Draw

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (randomNumber1 > randomNumber2) {
 } else if ( randomNumber1 < randomNumber2 ) {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
 } else {
-    document.querySelector("h1").innerHTML = "Draw"
+    document.querySelector("h1").innerHTML = "Draw!"
 }
 
 // button play again
